f6f1b72c0054525de6e17df003e86ca1f565ab96
ZiyueZHENG <75109830+ZiyueZHENG@users.noreply.github.com>
2021-11-08 20:44:57 -0600

    Add files via upload

---
+++ code/data_process.py
@@ -0,0 +1,16 @@
+import json
+import pandas as pd
+
+data = []
+with open('./yelp_dataset/business.json','rb') as f:
+    while True:
+        line_data = f.readline()
+        if not line_data:
+            break
+        temp = json.loads(line_data)
+        categories = str(temp['categories'])
+        if 'Sandwiches' in categories:
+            data.append(temp)
+data = pd.DataFrame(data)
+data.to_csv('./sandwiches.csv')
+
