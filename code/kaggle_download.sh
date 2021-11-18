db8e8e241a0f12313bbf210e098639b135f5041c
Ouyang <oxu2@wisc.edu>
2021-11-18 12:54:00 -0600

    Update kaggle_download.sh

--- code/kaggle_download.sh
+++ code/kaggle_download.sh
@@ -15,10 +15,19 @@
 
 ~/.local/bin/kaggle datasets download -f amazon_reviews_us_Mobile_Apps_v1_00.tsv  cynthiarempel/amazon-us-customer-reviews-dataset
 
-#404
-~/.local/bin/kaggle datasets download -f  amazon_reviews_us_PC_v1_00.tsv cynthiarempel/amazon-us-customer-reviews-dataset
+~/.local/bin/kaggle datasets download -f amazon_reviews_us_Digital_Ebook_Purchase_v1_01.tsv cynthiarempel/amazon-us-customer-reviews-dataset
 
 #404
-~/.local/bin/kaggle datasets download -f  amazon_reviews_us_Shoes_v1_00.tsv cynthiarempel/amazon-us-customer-reviews-dataset
+# ~/.local/bin/kaggle datasets download -f  amazon_reviews_us_PC_v1_00.tsv cynthiarempel/amazon-us-customer-reviews-dataset
 
-~/.local/bin/kaggle datasets download -f amazon_reviews_us_Shoes_v1_00.tsv cynthiarempel/amazon-us-customer-reviews-dataset
+#404
+# ~/.local/bin/kaggle datasets download -f  amazon_reviews_us_Shoes_v1_00.tsv cynthiarempel/amazon-us-customer-reviews-dataset
+
+
+# unzip
+unzip amazon_reviews_us_Books_v1_02.tsv.zip
+unzip amazon_reviews_us_Camera_v1_00.tsv.zip
+unzip amazon_reviews_us_Digital_Video_Download_v1_00.tsv.zip
+unzip amazon_reviews_us_Electronics_v1_00.tsv.zip
+unzip amazon_reviews_us_Mobile_Apps_v1_00.tsv.zip
+unzip amazon_reviews_us_Digital_Ebook_Purchase_v1_01.tsv.zip
