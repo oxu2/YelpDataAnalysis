5283edd39194c1cfcfb3fa2647e151d5989699be
Ouyang <oxu2@wisc.edu>
2021-11-18 09:41:06 -0600

    Update kaggle_download.sh

--- code/kaggle_download.sh
+++ code/kaggle_download.sh
@@ -20,3 +20,5 @@
 
 #404
 ~/.local/bin/kaggle datasets download -f  amazon_reviews_us_Shoes_v1_00.tsv cynthiarempel/amazon-us-customer-reviews-dataset
+
+~/.local/bin/kaggle datasets download -f amazon_reviews_us_Shoes_v1_00.tsv cynthiarempel/amazon-us-customer-reviews-dataset
