621cf593ad9a999536971dd0c315928c15f9bbc8
Ouyang <oxu2@wisc.edu>
2021-11-18 09:37:55 -0600

    Update kaggle_download.sh

--- code/kaggle_download.sh
+++ code/kaggle_download.sh
@@ -1,21 +1,22 @@
 # For the whole set
 # ~/.local/bin/kaggle datasets download -d cynthiarempel/amazon-us-customer-reviews-dataset
 
+
 # Particular File
+
 ~/.local/bin/kaggle datasets download -f amazon_reviews_us_Books_v1_02.tsv cynthiarempel/amazon-us-customer-reviews-dataset
 
 ~/.local/bin/kaggle datasets download -f amazon_reviews_us_Camera_v1_00.tsv cynthiarempel/amazon-us-customer-reviews-dataset
 
 ~/.local/bin/kaggle datasets download -f amazon_reviews_us_Digital_Video_Download_v1_00.tsv cynthiarempel/amazon-us-customer-reviews-dataset
 
-#404
-# ~/.local/bin/kaggle datasets download -f amazon_reviews_us_Electronics_v1_00.tsv cynthiarempel/amazon-us-customer-reviews-dataset
 
 ~/.local/bin/kaggle datasets download -f amazon_reviews_us_Electronics_v1_00.tsv cynthiarempel/amazon-us-customer-reviews-dataset
 
 ~/.local/bin/kaggle datasets download -f amazon_reviews_us_Mobile_Apps_v1_00.tsv  cynthiarempel/amazon-us-customer-reviews-dataset
 
 #404
-~/.local/bin/kaggle datasets download -f  amazon_reviews_us_Video_Games_v1_00.tsv cynthiarempel/amazon-us-customer-reviews-dataset
+~/.local/bin/kaggle datasets download -f  amazon_reviews_us_PC_v1_00.tsv cynthiarempel/amazon-us-customer-reviews-dataset
 
+#404
 ~/.local/bin/kaggle datasets download -f  amazon_reviews_us_Shoes_v1_00.tsv cynthiarempel/amazon-us-customer-reviews-dataset
