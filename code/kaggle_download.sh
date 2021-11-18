63dd6b75af4347feef5446e7b938a5d0d8fbf65a
Ouyang <oxu2@wisc.edu>
2021-11-18 09:01:49 -0600

    Create kaggle_download.sh

---
+++ code/kaggle_download.sh
@@ -0,0 +1,17 @@
+# For the whole set
+# kaggle datasets download -d cynthiarempel/amazon-us-customer-reviews-dataset
+
+# Particular File
+~/.local/bin/kaggle datasets download -f amazon_reviews_us_Books_v1_02.tsv cynthiarempel/amazon-us-customer-reviews-dataset
+
+~/.local/bin/kaggle datasets download -f amazon_reviews_us_Camera_v1_00.tsv cynthiarempel/amazon-us-customer-reviews-dataset
+
+~/.local/bin/kaggle datasets download -f amazon_reviews_us_Digital_Video_Download_v1_00.tsv cynthiarempel/amazon-us-customer-reviews-dataset
+
+~/.local/bin/kaggle datasets download -f amazon_reviews_us_Electronics_v1_00.csv cynthiarempel/amazon-us-customer-reviews-dataset
+
+~/.local/bin/kaggle datasets download -f amazon_reviews_us_Electronics_v1_00.tsv  cynthiarempel/amazon-us-customer-reviews-dataset
+
+~/.local/bin/kaggle datasets download -f   cynthiarempel/amazon-us-customer-reviews-dataset
+
+~/.local/bin/kaggle datasets download -f   cynthiarempel/amazon-us-customer-reviews-dataset
