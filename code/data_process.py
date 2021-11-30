8d3d4bcac0924ccbdd8d3a10dd5969d2fe0e700b
ZiyueZHENG <75109830+ZiyueZHENG@users.noreply.github.com>
2021-11-30 17:59:08 -0600

    Update data_process.py

--- code/data_process.py
+++ code/data_process.py
@@ -9,7 +9,7 @@ with open('./yelp_dataset/business.json','rb') as f:
             break
         temp = json.loads(line_data)
         categories = str(temp['categories'])
-        if 'Sandwiches' in categories:
+        if ('Sandwiches' in categories or 'Burgers' in categories) and ('Pizza' not in categories) and ('Barbecue' not in categories) and ('Barbeque' not in categories):
             data.append(temp)
 data = pd.DataFrame(data)
 data.to_csv('./sandwiches.csv')
